0dd123ae230477dde060b8b64745625c8574a6f3
Michael Sklepowich <michael@sklepowich.com>
2018-10-16 11:19:53 -0700

    added logic for reformatting shifts by days of the week sorted by start_time

--- employee_dashboard/views.py
+++ employee_dashboard/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from .models import User_Schedule
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.core.exceptions import PermissionDenied
+from .admin_dashboard.models import Shift
 
 
-def User_Schedule_view(request):
-    u_sched = User_Schedule.objects.all()
+def employee_dashboard_view(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied
+
+
+    shift = request.user.selected_shift
 
     context = {
-        'models': u_sched
+        'shift'
+        'calender': notes
+
     }
-    return render(request, 'models/models_list.html', context=context)
+
+    return render(request, 'notes/notes_list.html', context=context)
+
+
+def notes_detail_view(request, pk=None):
+    if not request.user.is_authenticated:
+        return redirect(reverse('login'))
+
+    note = get_object_or_404(Note, id=pk, user__id=request.user.id)
+    context = {
+        'note': note,
+    }
+
+    return render(request, 'notes/notes_detail.html', context)
