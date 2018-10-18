c0fcaff513b455fa6330791fe78fe34dee67fd59
chris turner <-v>
2018-10-18 10:44:08 -0700

    clean user form data
    make sure we have their prefs in a dict

--- employee_dashboard/views.py
+++ employee_dashboard/views.py
@@ -26,7 +26,9 @@ def employee_dashboard_view(request):
             'start_time': shift.start_time,
             'end_time': shift.end_time,
             'employees_required': shift.employees_required,
-            'employees_assigned': shift.employees_assigned
+            'employees_assigned': shift.employees_assigned,
+            'id': shift.id,
+            'day': shift.day,
         }
         days_of_the_week[shift.day.lower()].append(new_shift)
 
@@ -34,6 +36,27 @@ def employee_dashboard_view(request):
     for day in days_of_the_week:
         days_of_the_week[day].sort(key=itemgetter('start_time'))
 
+    # [[monday-3, 1],monday-4=,monday-2=,monday-1=,tuesday-6=2,wednesday-5=3]
+    if 'list' in request.POST:
+        decoded_prefs = request.body.decode().split('&')
+        split_prefs = []
+        chosen_user_prefs = []
+        true_prefs = []
+
+        for i in decoded_prefs:
+            split_prefs.append(i.split('='))
+        for i in split_prefs:
+            chosen_user_prefs.append(i)
+
+        chosen_user_prefs.pop(0)
+        chosen_user_prefs.pop(-1)
+
+        for i in chosen_user_prefs:
+            if i[1]:
+                true_prefs.append(i)
+
+        true_prefs = dict(true_prefs)
+    import pdb; pdb.set_trace()
     context = {
         'calendar': days_of_the_week,
         # 'all_projects': all_shifts
